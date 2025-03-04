11f1aaca428b460ec5bc8f05873559da6aa5ea31
Microseversk <alexander.osipov.work@gmail.com>
2025-03-04 20:13:10 +0700

    main 🦝 fix proxy

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('core');
 
   const config = new DocumentBuilder()
     .setTitle('Scam Bank Core Service')
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory());
+  SwaggerModule.setup('core/docs', app, documentFactory());
 
-  await app.listen(process.env.PORT ?? 1337);
-  console.log(`Server is running on port ${process.env.PORT ?? 1337}`);
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
 
 bootstrap();
